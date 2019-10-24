66930e2b0cbda4f417289042a8bab6da70a5d683
Lam Le <15520405@gm.uit.edu.vn>
2019-10-24 22:59:50 +0700

    Added swagger and Ethereum settings

---
+++ Example-Ethereum-Dapp-Backend/EthereumSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Example_Ethereum_Dapp_Backend
+{
+    internal class EthereumSettings
+    {
+        public string EthereumAccount { get; set; }
+        public string EthereumPassword { get; set; }
+
+        public string Abi { get; set; }
+        public string ContractAddress { get; set; }
+    }
+}
--- Example-Ethereum-Dapp-Backend/Startup.cs
+++ Example-Ethereum-Dapp-Backend/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -10,11 +11,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Example_Ethereum_Dapp_Backend
 {
     public class Startup
     {
+        private readonly string CorsPolicy = "CorsPolicy";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +30,33 @@ namespace Example_Ethereum_Dapp_Backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var ethereumSettings = Configuration.GetSection("EthereumSettings");
+            services.Configure<EthereumSettings>(ethereumSettings);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "Server API",
+                    Version = "v1"
+                });
+
+                var xmlFile = Path.ChangeExtension(typeof(Startup).Assembly.Location, ".xml");
+                c.IncludeXmlComments(xmlFile);
+            });
+
+
+            services.AddCors(o =>
+            {
+                o.AddPolicy(CorsPolicy,
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +71,18 @@ namespace Example_Ethereum_Dapp_Backend
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors(CorsPolicy);
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
