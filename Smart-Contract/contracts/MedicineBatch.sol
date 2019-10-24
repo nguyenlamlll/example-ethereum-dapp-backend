5a471781a23b3d93daba2469179502d60e50e89d
Lam Le <15520405@gm.uit.edu.vn>
2019-10-25 00:02:26 +0700

    Added smart contracts

---
+++ Smart-Contract/contracts/MedicineBatch.sol
@@ -0,0 +1,58 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+import './Ownable.sol';
+
+contract MedicineBatch is Ownable {
+
+    enum TypesOfMedicine { Pill, Bottle }
+    
+    string public guid;
+    string public name;
+    string public registrationCode;
+    string public batchNumber;
+    uint public quantity;
+    uint public manufacturingDate;
+    uint public expiryDate;
+    TypesOfMedicine public typeOfMedicine;
+    
+    constructor(
+        string memory _guid,
+        string memory _name,
+        string memory _registrationCode,
+        string memory _batchNumber,
+        uint _quantity,
+        uint _manufacturingDate,
+        uint _expiryDate,
+        TypesOfMedicine _typeOfMedicine)
+        public
+    {
+        guid = _guid;
+        name = _name;
+        registrationCode = _registrationCode;
+        batchNumber = _batchNumber;
+        quantity = _quantity;
+        manufacturingDate = _manufacturingDate;
+        expiryDate = _expiryDate;
+        typeOfMedicine = _typeOfMedicine;
+    }
+    
+    function updateMedicineBatchInformations(
+        string memory _name,
+        string memory _registrationCode,
+        string memory _batchNumber,
+        uint _quantity,
+        uint _manufacturingDate,
+        uint _expiryDate,
+        TypesOfMedicine _typeOfMedicine) 
+        public 
+        onlyOwner 
+    {
+        name = _name;
+        registrationCode = _registrationCode;
+        batchNumber = _batchNumber;
+        quantity = _quantity;
+        manufacturingDate = _manufacturingDate;
+        expiryDate = _expiryDate;
+        typeOfMedicine = _typeOfMedicine;
+    }
+}
